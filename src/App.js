4131a19ad1cbf0211f38823084b89ab38616d12d
grant kinghorn <gkinghorn3@icloud.com>
2023-07-13 22:31:18 +0100

    date component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
   const expenses = [
@@ -15,21 +16,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+      <ExpenseList expenses={expenses} />
     </div>
   );
 }
