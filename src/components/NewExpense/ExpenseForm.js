97b8cfb7d4037bab5c00d4610ff11708302f9e78
Grant <gkinghorn3@icloud.com>
2023-07-20 15:03:43 +0100

    input data collected & two way bindings added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -9,6 +9,7 @@ const ExpenseForm = (props) => {
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+   
     }
 
     const amountChangeHandler = (event) => {
@@ -19,20 +20,35 @@ const ExpenseForm = (props) => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-  return <form>
+        const enteredData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        }
+
+        console.log(enteredData);
+        setEnteredTitle('');
+        setEnteredAmount('');   
+        setEnteredDate('');
+
+    };
+
+  return <form onSubmit={submitHandler}>
     <div className='new-expense__controls'>
         <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+            <input type='text' onChange={titleChangeHandler} value={enteredTitle}/>
         </div>
         <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+            <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} value={enteredAmount}/>
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler}/>
+            <input type='date' min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler} value={enteredDate}/>
         </div>
     </div>
     <div className='new-expense__actions'>
