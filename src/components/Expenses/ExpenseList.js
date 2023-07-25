f7c476558324c93a8d5dd85022a989ffaf7fe545
grant kinghorn <gkinghorn3@icloud.com>
2023-07-25 22:50:43 +0100

    m

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
@@ -8,6 +8,7 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 const ExpenseList = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
+    const [filterInfoText, setFilterInfoText] = useState('2019, 2021, 2022');
 
     const selectDateHandler = (selectedDate) => {
         setFilteredYear(selectedDate);
@@ -16,6 +17,7 @@ const ExpenseList = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selectedYear={filteredYear} onSelectDate={selectDateHandler} />
+      <p>Data for years {filterInfoText} is hidden</p>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
