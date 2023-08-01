e82988fd6303085b8eb9839f0d06f01a9b105b95
Grant <gkinghorn3@icloud.com>
2023-08-01 12:50:00 +0100

    dynamic list updating

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
+import React, { useState } from "react";
+
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+
+const initialExpenses = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 60,
+    date: new Date(2022, 2, 12),
+  },
+  { id: "e2", title: "Fuel", amount: 120, date: new Date(2022, 2, 8) },
+  { id: "e3", title: "Gym", amount: 42, date: new Date(2019, 2, 1) },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 60,
-      date: new Date(2022, 2, 12),
-    },
-    { id: "e2", title: "Fuel", amount: 120, date: new Date(2022, 2, 8) },
-    { id: "e3", title: "Gym", amount: 42, date: new Date(2019, 2, 1) },
-  ];
+
+  const [expenses, setExpenses] = useState(initialExpenses);
+
 
   const addExpenseHandler = (expense) => {
-    console.log("in app.js");
-    console.log(expense);
+    setExpenses(initialExpenses => {
+      return [expense, ...initialExpenses];
+    });
   };
 
   return (
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -7,10 +7,11 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 const ExpenseList = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  const [filterInfoText, setFilterInfoText] = useState("2019, 2021, 2022");
+
 
   const selectDateHandler = (selectedDate) => {
     setFilteredYear(selectedDate);
+    
   };
 
   return (
@@ -19,7 +20,7 @@ const ExpenseList = (props) => {
         selectedYear={filteredYear}
         onSelectDate={selectDateHandler}
       />
-      <p>Data for years {filterInfoText} is hidden</p>
+
       {props.expenses.map(expense => (
         <ExpenseItem
           title={expense.title}
